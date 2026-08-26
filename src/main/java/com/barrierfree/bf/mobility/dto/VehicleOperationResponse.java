05c953af0989a117ceaeb6566ca81f38ea72f50a
김수빈 <bella8010@naver.com>
2026-08-26 15:20:34 +0900

    fix:spotless 재수정

--- src/main/java/com/barrierfree/bf/mobility/dto/VehicleOperationResponse.java
+++ src/main/java/com/barrierfree/bf/mobility/dto/VehicleOperationResponse.java
@@ -11,24 +11,22 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class VehicleOperationResponse {
 
-    private String centerName;
-    private String vehicleId;
-    private String vehicleModelName;
-    private String vehicleUsageName;
-    private boolean isOperationAvailable;
-    private String baseDateTime;
+  private String centerName;
+  private String vehicleId;
+  private String vehicleModelName;
+  private String vehicleUsageName;
+  private boolean isOperationAvailable;
+  private String baseDateTime;
 
-    /**
-     * 외부 API의 Raw Item 데이터를 우리 서비스 규격의 응답 DTO로 변환합니다.
-     */
-    public static VehicleOperationResponse from(VehicleOperationRawResponse.Item rawItem) {
-        return VehicleOperationResponse.builder()
-            .centerName(rawItem.getCntrNm())
-            .vehicleId(rawItem.getVhclId())
-            .vehicleModelName(rawItem.getVhclMdlNm())
-            .vehicleUsageName(rawItem.getVhclUsgNm())
-            .isOperationAvailable("Y".equalsIgnoreCase(rawItem.getOperSttsYn()))
-            .baseDateTime(rawItem.getTotDt())
-            .build();
-    }
+  /** 외부 API의 Raw Item 데이터를 우리 서비스 규격의 응답 DTO로 변환합니다. */
+  public static VehicleOperationResponse from(VehicleOperationRawResponse.Item rawItem) {
+    return VehicleOperationResponse.builder()
+        .centerName(rawItem.getCntrNm())
+        .vehicleId(rawItem.getVhclId())
+        .vehicleModelName(rawItem.getVhclMdlNm())
+        .vehicleUsageName(rawItem.getVhclUsgNm())
+        .isOperationAvailable("Y".equalsIgnoreCase(rawItem.getOperSttsYn()))
+        .baseDateTime(rawItem.getTotDt())
+        .build();
+  }
 }
--- src/main/java/com/barrierfree/bf/mobility/dto/external/VehicleUseRawResponse.java
+++ src/main/java/com/barrierfree/bf/mobility/dto/external/VehicleUseRawResponse.java
@@ -11,51 +11,55 @@ import lombok.NoArgsConstructor;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class VehicleUseRawResponse {
 
+  private Header header;
+  private Body body;
+  private ResponseWrapper response;
+
+  public Header getHeaderSafe() {
+    return (this.response != null && this.response.getHeader() != null)
+        ? this.response.getHeader()
+        : this.header;
+  }
+
+  public Body getBodySafe() {
+    return (this.response != null && this.response.getBody() != null)
+        ? this.response.getBody()
+        : this.body;
+  }
+
+  @Getter
+  @NoArgsConstructor
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public static class ResponseWrapper {
     private Header header;
     private Body body;
-    private ResponseWrapper response;
+  }
 
-    public Header getHeaderSafe() {
-        return (this.response != null && this.response.getHeader() != null) ? this.response.getHeader() : this.header;
-    }
+  @Getter
+  @NoArgsConstructor
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public static class Header {
+    private String resultCode;
+    private String resultMsg;
+  }
 
-    public Body getBodySafe() {
-        return (this.response != null && this.response.getBody() != null) ? this.response.getBody() : this.body;
-    }
+  @Getter
+  @NoArgsConstructor
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public static class Body {
+    @JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
+    private List<Item> item;
+  }
 
-    @Getter
-    @NoArgsConstructor
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class ResponseWrapper {
-        private Header header;
-        private Body body;
-    }
-
-    @Getter
-    @NoArgsConstructor
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class Header {
-        private String resultCode;
-        private String resultMsg;
-    }
-
-    @Getter
-    @NoArgsConstructor
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class Body {
-        @JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
-        private List<Item> item;
-    }
-
-    @Getter
-    @NoArgsConstructor
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class Item {
-        private String totDt;
-        private String cntrNm;
-        private String tvhclCntom;
-        private String oprVhclCntom;
-        private String avlVhclCntom;
-        private String wtngNocs;
-    }
+  @Getter
+  @NoArgsConstructor
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public static class Item {
+    private String totDt;
+    private String cntrNm;
+    private String tvhclCntom;
+    private String oprVhclCntom;
+    private String avlVhclCntom;
+    private String wtngNocs;
+  }
 }
